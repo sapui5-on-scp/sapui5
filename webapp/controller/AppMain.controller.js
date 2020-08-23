bbe835b2998f7705a4a110be2e4228ea6f50b1b9
sapui5-on-scp <70101455+sapui5-on-scp@users.noreply.github.com>
2020-08-23 23:33:58 +0900

    Add files via upload

--- webapp/controller/AppMain.controller.js
+++ webapp/controller/AppMain.controller.js
@@ -1,1 +1,15 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
 
+	return Controller.extend("ds.test.controller.AppMain", {
+		onInit: function () {
+			console.log("onInit AppMain");
+		},
+
+		onPressBtn: function () {
+			console.log("!!!!");
+		}
+	});
+});
